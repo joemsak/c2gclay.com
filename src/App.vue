5dad2439df59980859c374beebe486c8b44fb5ff
Joe Sak <joe@joesak.com>
2018-12-05 12:00:12 -0600

    Initial construction commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Under Construction" src="./assets/construction.gif">
+    <p>C2G Clay is coming soon...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
