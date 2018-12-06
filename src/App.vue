35946401cc3f6c53a3e83595d12c5697cf1e26b3
Joe Sak <joe@joesak.com>
2018-12-06 14:46:23 -0600

    Center the logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app" class="container-fluid mt-3 mx-auto">
-    <img alt="c2g logo" src="./assets/logo.png">
+  <div id="app" class="container-fluid mt-5">
+    <div style="width: 227px;" class="mx-auto">
+      <img alt="c2g logo" width="227" height="227" src="./assets/logo.png">
+    </div>
   </div>
 </template>
 
