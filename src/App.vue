3bb6830957f842821708a98f4a502cb6b626d01b
Joe Sak <joe@joesak.com>
2019-02-14 22:41:05 -0600

    Add first gallery photo, logo resize effect on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app" class="container-fluid mt-5">
-    <div style="width: 227px;" class="mx-auto">
-      <img
-        alt="c2g logo"
-        width="227"
-        height="227"
-        src="./assets/logo-white-drop-shadow.png"
-      />
+  <div id="app" class="container-fluid">
+    <div id="logo-wrapper">
+      <div id="logo" class="mt-5"></div>
+    </div>
+
+
+    <div id="cover"></div>
+
+    <div class="gallery-img">
+      <img width="100%" src="./assets/galleries/vajilla-ricky/DSCF8208.jpg" />
     </div>
   </div>
 </template>
@@ -14,6 +16,26 @@
 <script>
 export default {
   name: 'app',
+
+  created() {
+    window.onscroll = function() { scrollFunction() }
+
+    function scrollFunction() {
+      const logo = document.querySelector("#logo")
+
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        logo.style.width = "50px"
+        logo.style.height = "50px"
+        logo.classList.remove("mt-5")
+        logo.classList.add("ml-3", "mt-3")
+      } else {
+        logo.style.width = "250px"
+        logo.style.height = "250px"
+        logo.classList.add("mt-5")
+        logo.classList.remove("ml-3", "mt-3")
+      }
+    }
+  }
 }
 </script>
 
